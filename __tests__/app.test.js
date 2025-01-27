262485eb90c629dc4f13d5c0b0bf26d10c9fcfa3
scottgirling <scottgirling0812@gmail.com>
2025-01-27 15:09:24 +0000

    add GET api/topics endpoint to find all topics, with tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,11 @@
 const request = require("supertest");
 const app = require("../db/app");
+const connection = require("../db/connection");
 const endpointsJson = require("../endpoints.json");
+
+afterAll(() => {
+  return connection.end();
+})
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -10,5 +15,20 @@ describe("GET /api", () => {
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
       });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects and an appropriate status code", () => {
+    return request(app)
+    .get("/api/topics")
+    .expect(200)
+    .then(({ body: { topics } }) => {
+      expect(topics.length).toBe(3);
+      topics.forEach((topic) => {
+        expect(topic).toHaveProperty("slug");
+        expect(topic).toHaveProperty("description");
+      });
+    });
   });
 });
