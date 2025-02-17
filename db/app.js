5babbd7e46e36d7ffa451e46fc0fc0eb4f227e77
scottgirling <scottgirling0812@gmail.com>
2025-02-17 10:54:05 +0000

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const apiRouter = require("./routes/api-router");
+
+app.use(cors());
 
 app.use(express.json());
 
